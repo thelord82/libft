5a63db125dee5123825eb95fc3efc7cafaa3defb
thelord82 <mandrelord@gmail.com>
2022-04-08 21:19:13 -0400

    AAA on WM, KO 23-26 Tripouille, OK Unit, OK FT_SP

--- ft_split.c
+++ ft_split.c
@@ -6,16 +6,17 @@
 /*   By: malord <malord@student.42quebec.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 10:24:11 by malord            #+#    #+#             */
-/*   Updated: 2022/04/08 17:25:12 by malord           ###   ########.fr       */
+/*   Updated: 2022/04/08 21:09:09 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 static int	ft_wordcount(char const *s, char c)
 {
-	size_t	i;
-	size_t	w;
+	int	i;
+	int	w;
 
 	i = 0;
 	w = 0;
@@ -30,7 +31,18 @@ static int	ft_wordcount(char const *s, char c)
 
 static int	ft_wordlen(char const *s, char c, int cpt)
 {
-	size_t	l;
+	int	l;
+
+	l = 0;
+	while (s[cpt] != c && s[cpt] != '\0')
+	{
+		l++;
+		cpt++;
+	}
+	return (l);
+}
+/*{
+	int	l;
 
 	l = 0;
 	while (cpt >= 0 && s[cpt] != c)
@@ -39,11 +51,11 @@ static int	ft_wordlen(char const *s, char c, int cpt)
 		l++;
 	}
 	return (l);
-}
+}*/
 
 static	char	*ft_trimword(char const *s, char c, int cpt)
 {
-	size_t	j;
+	int	j;
 	char	*nstr;
 
 	j = 0;
@@ -66,21 +78,52 @@ static	char	*ft_trimword(char const *s, char c, int cpt)
 	}
 	return (nstr);
 }
+/*{
+	int	i;
+	int	wlen;
+	int	j;
+	char	*word;
+
+	i = 0;
+	wlen = ft_wordlen(s, c, cpt);
+	j = (cpt - wlen) + 1;
+	word = ft_calloc(wlen + 1, sizeof(char));
+	while (j <= cpt)
+	{
+		word[i] = s[j];
+		i++;
+		j++;
+	}
+	return (word);
+}*/
 
 char	**ft_split(char const *s, char c)
 {
 	char	**result;
-	size_t	i;
-	size_t	j;
+	int	i;
+	int	j;
+	int index;
 
 	if (!s)
 		return (NULL);
 	result = ft_calloc(ft_wordcount(s, c) + 1, sizeof(char *));
 	i = 0;
 	j = 0;
+	index = -1;
 	if (!result)
 		return (NULL);
-	while (s[i])
+	while (i <= (int)ft_strlen(s))
+	{
+		if (s[i] != c && index < 0)
+			index = i;
+		else if ((s[i] == c || i == (int)ft_strlen(s)) && index >= 0)
+		{
+			result[j++] = ft_trimword(s, c, index);
+			index = -1;
+		}
+		i++;
+	}
+	/*while (s[i])
 	{
 		if (s[i] != c)
 		{
@@ -91,9 +134,16 @@ char	**ft_split(char const *s, char c)
 			}
 		}
 		i++;
-	}
+	}*/
 	return (result);
 }
+/*int main()
+{
+	printf("%d\n", ft_wordlen("bleu blanc rouge", ' ', 11));
+	printf("%d\n", ft_wordcount("bleu blanc rouge", ' '));
+	printf("%s\n", ft_trimword("bleu blanc rouge", ' ', 12));
+	//printf("%d\n", ft_wordlens("bleu blanc rouge", 11, ' '));
+}*/
 /*result[j++] = ft_trimword(s, c, i);
 			while (s[i] != c && s[i] != '\0')
 				i++;
@@ -104,3 +154,13 @@ char	**ft_split(char const *s, char c)
 			i++;
 	}
 	return (result);*/
+
+	/*if (s[i] != c)
+		{
+			if (s[i + 1] == c || s[i + 1] == '\0')
+			{
+				result[j] = ft_trimword(s, c, i);
+				j++;
+			}
+		}
+		i++;*/
