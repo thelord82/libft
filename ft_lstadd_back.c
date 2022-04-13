923ebf21f19afb47c9ca672ad36b4563f0fe480e
thelord82 <mandrelord@gmail.com>
2022-04-12 20:27:14 -0400

    MAJ - lstadd_back bug

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -5,12 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: malord <malord@student.42quebec.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/12 16:10:17 by malord            #+#    #+#             */
-/*   Updated: 2022/04/12 16:59:15 by malord           ###   ########.fr       */
+/*   Created: 2022/04/12 20:23:36 by malord            #+#    #+#             */
+/*   Updated: 2022/04/12 20:23:57 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include"libft.h"
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
@@ -20,7 +20,7 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		*lst = new;
 	else
 	{
-		last = ft_lstlast(*(lst));
+		last = ft_lstlast(*lst);
 		last->next = new;
 	}
 }
